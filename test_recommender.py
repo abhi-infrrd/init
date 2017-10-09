82d392ec4fe4d4ea19af80d3bcf1c4458b0811f0
abhi-infrrd <abhishekjha@infrrd.ai>
2017-10-09 18:01:20 +0530

    removed a file usage

--- test_recommender.py
+++ test_recommender.py
@@ -5,7 +5,7 @@ Created on Fri Oct  6 12:55:12 2017
 @author: Abhishek
 """
 import os
-os.chdir('C:\\Users\\user\\Desktop\\test_panda') #harcoded
+os.chdir('C:\\Users\\user\\Desktop\\test_panda')
 import Recommender as rc
 import pandas as pd
 from scipy.spatial.distance import cosine
@@ -14,14 +14,15 @@ from collections import OrderedDict
 data = ''
 
 d1 = ''
-
+class NoRecordFoundException(Exception):
+    pass
 def preprocess_data():
     
     global data
     
-    data = rc.read_a_file_to_dataframe("C:\\Users\\user\\Desktop\\test_panda\\Products_09202017.txt",'\t') #harcoded
+    data = rc.read_a_file_to_dataframe("C:\\Users\\user\\Desktop\\test_panda\\Products_09202017.txt",'\t') 
 
-    columns = [0,1,2,3,4,5,13,14,15] #harcoded
+    columns = [0,1,2,3,4,5,13,14,15]
 
     global d1
 
@@ -42,10 +43,10 @@ def tf_idf_proceessing(attribute):
     x,v = rc.proc_an_attribute(d1,attribute)
     arr = v.get_feature_names()
     
-    rc.write_attributes_to_a_file('desc.txt',arr) #harcoded
-    rc.write_values_to_a_file('desc.txt',list(x.toarray())) #harcoded
+    rc.write_attributes_to_a_file('desc.txt',arr) 
+    rc.write_values_to_a_file('desc.txt',list(x.toarray())) 
 
-    data_frame_temp  = rc.read_a_file_to_dataframe("C:\\Users\\user\\Desktop\\test_panda\\desc.txt",'\t') #harcoded
+    data_frame_temp  = rc.read_a_file_to_dataframe("C:\\Users\\user\\Desktop\\test_panda\\desc.txt",'\t') 
     data_frame_temp.columns = data_frame_temp.iloc[0]
     data_frame_temp.drop(data_frame_temp.index[[0,0]], inplace=True)
     
@@ -61,19 +62,13 @@ def one_hot_encode_processing(attribute):
     global d1
     
     d1 = rc.one_hot_encoding(d1,attribute)
-    
-
-
-#d1.to_csv('D:\\prod_data\prod.txt', sep='\t')
-#d1.iloc[:,3:].to_csv('D:\\prod_data\k.txt', sep='\t') #filtered columns to find correlation with
-
 
 def initialise():
     preprocess_data()    
 
-    tf_idf_proc_list = ['Keywords','Topic','Title','Description'] #harcoded
+    tf_idf_proc_list = ['Keywords','Topic','Title','Description']
 
-    one_hot_encode_list = ['Type','PriceRange'] #harcoded
+    one_hot_encode_list = ['Type','PriceRange'] 
 
     for i in tf_idf_proc_list:
         tf_idf_proceessing(i)
@@ -90,24 +85,21 @@ def recommend():
     global d1
     product_code = input("Enter a product code")
     record = d1.loc[d1['ProductCode'] == product_code]
+    if(len(record.index)==0):
+        raise NoRecordFoundException('No record for the given product-code')
     record = record.iloc[:1,3:]
-    #d1 = d1[d1.ProductCode != product_code]
     return record
 def cos_recommendation():
     global d1
     record = recommend()
-    #f = open('cos.txt','w')
     l = []
     loop = len(d1.index)
     for i in range(loop+2):
         try:
             k = 1-cosine(record,d1.iloc[i:i+1,3:])
             l.append(k)
-            #f.write(str(k))
-            #f.write('\n')
         except:
             break
-    #f.close()
     mapping  = d1.iloc[:,:1]
 
     df = pd.DataFrame(l,columns=['rating'])
@@ -120,33 +112,11 @@ def cos_recommendation():
     k = ordered.keys()
     for i in k:
         l1.append(i)
-    
-    l2 =[]
-    k = ordered.values()
-    for i in k:
-        l2.append(i['rating'])
-    f = open('a.txt','w')
-    for i,j in zip(reversed(l1),reversed(l2)):
-        f.write(str(i))
-        f.write('\t')
-        f.write(str(j))
-        f.write('\n')
-    f.close()
-
-    f = open('a.txt','r')
-    l =[]
-    for i in f:
-        l.append(i)
+    l1.reverse()
     num = int(input('Enter the number of similar products you want to see'))
-
-    final_list = []
-
-    for i in l:
-        k = i.split('\t')
-        final_list.append(k[0])
     global data
     for i in range(num):
-        temp = data.loc[data['ProductCode'] == final_list[i]]
-        print(temp.iloc[:,:1])
+        temp = data.loc[data['ProductCode'] == l1[i]]
+        print(temp.iloc[:,:1]) #CHANGE SECOND SLICE TO GET MORE COLUMNS
 initialise()
 cos_recommendation()
