4511bf3f3fa0b60c3952e47eea94b1f64d80f395
abhi-infrrd <abhishekjha@infrrd.ai>
2017-10-11 21:23:36 +0530

    product_course api end point working

---
+++ product-course.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 17:04:00 2017
+
+@author: Abhishek Jha
+"""
+#This script produces the product-product and course-course recommendation. 
+
+#To run the script execute the following commands from the same directory
+#export FLASK_APP = api.py
+#python -m flask run --host=0.0.0.0 -p 2273
+
+from flask import Flask
+from flask import Response
+from flask import request
+import json
+import product_product as product_product_recommendation
+import courses_courses as course_course_recommendation
+app = Flask(__name__)
+
+cormatrix1 = ''
+
+cormatrix2 = ''
+
+#itemId is a query parameter
+#returns an array of json objects
+@app.route("/recommend/<project>/item", methods=['GET'])
+def get_project_records(project):
+  itemId = request.args.get('itemId')
+  
+  global cormatrix1
+  global cormatrix2
+  finaldata = ''
+  if(project=='CPA_PRODUCTS'):
+      finaldata = product_product_recommendation.getRecomendation(cormatrix1, itemId)
+  elif(project=='CPA_COURSES'):
+      finaldata = course_course_recommendation.getCourseRecomendation(cormatrix2, itemId)
+  else:
+      return Response(status=404, mimetype='application/json')
+  for temp in finaldata:
+    if(project=='CPA_PRODUCTS'):
+        temp['rItemId'] = temp['ProductCode']
+    elif(project=='CPA_COURSES'):
+        temp['rItemId'] = temp['CourseCode']
+  return Response(json.dumps(finaldata),  mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True,port=2273)
+    
+#Intial training on startup, delays the availability endpoint by a few minutes
+cormatrix1 = product_product_recommendation.populateRecomendations()
+cormatrix2 = course_course_recommendation.populateCourseRecomendations()
